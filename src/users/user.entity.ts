dbb7a79a1053c40674035a9e53a0708a59a1a569
cherry.a <cherry.a@kakaocorp.com>
2025-09-03 14:15:33 +0900

    53. 생성과 저장에 대한 추가 내용

--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -1,4 +1,13 @@
-import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  AfterInsert,
+  AfterRemove,
+  AfterUpdate,
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { IsEmail } from 'class-validator';
+import { IsString } from 'class-validator';
 
 @Entity()
 export class User {
@@ -6,8 +15,25 @@ export class User {
   id: number;
 
   @Column()
+  @IsEmail()
   email: string;
 
   @Column()
+  @IsString()
   password: string;
+
+  @AfterInsert()
+  logInsert() {
+    console.log('Inserted User with id', this.id);
+  }
+
+  @AfterUpdate()
+  logUpdate() {
+    console.log('Updated User with id', this.id);
+  }
+
+  @AfterRemove()
+  logRemove() {
+    console.log('Removed User with id', this.id);
+  }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,11 +2,19 @@ import { Injectable } from '@nestjs/common';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './user.entity';
+
 @Injectable()
 export class UsersService {
   constructor(@InjectRepository(User) private repo: Repository<User>) {}
 
   create(email: string, password: string) {
+    // TypeORM의 Repository.create() 메서드가 User 엔티티 타입을 알 수 있는 이유:
+    // 1. Repository<User>에서 <User> 제네릭 타입이 User 엔티티를 명시
+    // 2. TypeORM의 Repository<T>.create() 메서드는 T 타입의 인스턴스를 반환
+    // 3. NestJS의 @InjectRepository(User)가 User 엔티티에 대한 Repository를 생성할 때
+    //    User 엔티티의 메타데이터를 내부적으로 보관
+    // 4. create() 호출 시 보관된 User 엔티티 정보를 사용하여 User 인스턴스 생성
+    // 5. TypeScript 컴파일러가 제네릭 타입을 통해 타입 안전성 보장
     const user = this.repo.create({ email, password });
     return this.repo.save(user);
   }
