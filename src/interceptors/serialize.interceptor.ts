8a4fc05a4dd80e154524501c4072a0c00f910ba2
cherry.a <cherry.a@kakaocorp.com>
2025-09-10 12:57:04 +0900

    65 인터셉터 내 직렬화

--- src/interceptors/serialize.interceptor.ts
+++ src/interceptors/serialize.interceptor.ts
@@ -7,6 +7,7 @@ import {
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { plainToClass } from 'class-transformer';
+import { UserDto } from '../users/dtos/user.dto';
 
 // implements 는 추상 클래스나 인터페이스의 모든 조건을 만족하는 새로운 클래스를 만들때 사용함
 // extends 와는 다름.
@@ -14,12 +15,15 @@ export class SerializeInterceptor implements NestInterceptor {
   intercept(context: ExecutionContext, handler: CallHandler): Observable<any> {
     // run something before a request is handled
     // by the request handler
-    console.log('1. I am running before the handler', context);
+    // console.log('1. I am running before the handler', context);
 
     return handler.handle().pipe(
       map((data: any) => {
         // run something before the response is sent out
-        console.log('2. I am running before the response is sent out', data);
+        // console.log('3. I am running before the response is sent out', data);
+        return plainToClass(UserDto, data, {
+          excludeExtraneousValues: true, // exclude any properties that are not in the UserDto class
+        });
       }),
     );
   }
