d9495c889ab5b87fe57e5fcaf33849af6ba84794
cherry.a <cherry.a@kakaocorp.com>
2025-09-18 14:41:04 +0900

    77 세션 설정하기

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+// import { CookieSession } from 'cookie-session'
+const cookieSession = require('cookie-session');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(
+    cookieSession({
+      keys: ['asdfasdf'],
+    }),
+  );
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
