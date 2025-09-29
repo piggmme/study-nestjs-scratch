c03faf891ecc5554369ee00d8879845adec5cfbe
cherry.a <cherry.a@kakaocorp.com>
2025-09-29 19:10:08 +0900

    110 signin test

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -12,8 +12,9 @@ describe('UsersController', () => {
 
   beforeEach(async () => {
     fakeAuthService = {
-      // signin: (email: string, password: string) => {
-      // },
+      signin: (email: string, password: string) => {
+        return Promise.resolve({ id: 1, email, password } as User);
+      },
       // signup: (email: string, password: string) => {
       // },
     };
@@ -68,5 +69,18 @@ describe('UsersController', () => {
   it('findUser throws an error if user with given id is not found', async () => {
     fakeUsersService.findOne = () => null;
     await expect(controller.findUser('1')).rejects.toThrow(NotFoundException);
+  });
+
+  it('signin updates session object and returns user', async () => {
+    const session = { userId: -10 };
+    const user = await controller.signin(
+      {
+        email: 'asdf@asdf.com',
+        password: 'asdf',
+      },
+      session,
+    );
+    expect(user.id).toEqual(1);
+    expect(session.userId).toEqual(1);
   });
 });
