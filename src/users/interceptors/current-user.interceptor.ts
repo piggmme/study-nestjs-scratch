eb08be8bc52ced9d25014dd5048ca4461c153556
cherry.a <cherry.a@kakaocorp.com>
2025-10-01 15:20:31 +0900

    144 미들웨어, 가드, 인터셉터

--- src/users/interceptors/current-user.interceptor.ts
+++ src/users/interceptors/current-user.interceptor.ts
@@ -6,6 +6,11 @@ import {
 } from '@nestjs/common';
 import { UsersService } from '../users.service';
 
+// Even if we inject currentUser in a CurrentUserInterceptor,
+// we can’t use that data in middleware or guards
+// because middleward and guards run before interceptors.
+// So we should convert the CurrentUserInterceptor into middleware,
+// so guards can use currentUser as well.
 @Injectable()
 export class CurrentUserInterceptor implements NestInterceptor {
   constructor(private usersService: UsersService) {}
